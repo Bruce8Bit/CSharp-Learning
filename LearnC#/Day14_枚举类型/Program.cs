b4629d4e22754c1c71209476ade04ea4c7d65c41
Bruce8Bit <divexxd6@gmail.com>
2026-09-22 09:54:21 +0800

    枚举

---
+++ LearnC#/Day14_枚举类型/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day14_枚举类型
+{
+    enum weekDay
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            weekDay day = weekDay.Monday;
+            int d = (int)weekDay.Monday; //强制类型转换
+        }
+    }
+}
