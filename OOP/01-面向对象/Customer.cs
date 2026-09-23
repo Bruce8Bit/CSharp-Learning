cf2864813c5be1c58cefb101d91dd44731eec598
Bruce8Bit <divexxd6@gmail.com>
2026-09-23 09:43:32 +0800

    创建一个类

---
+++ OOP/01-面向对象/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_面向对象
+{
+    internal class Customer
+    {
+        public string name;
+        public string address;
+        public int age;
+        public string createTime;
+
+        public void Show() 
+        {
+            Console.WriteLine("姓名:" + name);
+            Console.WriteLine("地址:" + address);
+            Console.WriteLine("年龄:" + age);
+            Console.WriteLine("创建时间:" + createTime);
+        }
+    }
+}
